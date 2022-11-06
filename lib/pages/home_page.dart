3d1d51f336a35ddcdcb6b5b1ce2038359dbe831a
Shonu72 <shourya7250@gmail.com>
2022-11-06 12:56:53 +0530

    drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,12 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:veracity/auth/login_page.dart';
+import 'package:veracity/helper/helper_function.dart';
+import 'package:veracity/pages/profile_page.dart';
+import 'package:veracity/pages/search_page.dart';
 import 'package:veracity/services/auth_services.dart';
+import 'package:veracity/widgets/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,17 +16,138 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String userName = "";
+  String email = "";
   AuthServices authServices = AuthServices();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    gettingUserData();
+  }
+
+  gettingUserData() async {
+    await HelperFunction.getUserEmailFromSF().then((value) {
+      email = value!;
+    });
+    await HelperFunction.getUserNameFromSF().then((value) {
+      userName = value!;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: ElevatedButton(
-        child: const Text("LogOut"),
-        onPressed: () {
-          authServices.signOut();
-        },
-      )),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                nextScreen(context, const SearchPage());
+              },
+              icon: const Icon(Icons.search))
+        ],
+        centerTitle: true,
+        backgroundColor: Theme.of(context).primaryColor,
+        elevation: 0,
+        toolbarHeight: 80,
+        title: const Text(
+          "Groups",
+          style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 50),
+          children: <Widget>[
+            const Icon(
+              Icons.account_circle_rounded,
+              size: 150,
+              color: Colors.blueGrey,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Text(
+              userName,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ListTile(
+              onTap: () {},
+              selectedColor: Theme.of(context).primaryColor,
+              selected: true,
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              leading: Icon(Icons.groups_rounded),
+              title: const Text(
+                "Groups",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+            ListTile(
+              onTap: () {
+                nextScreen(context, ProfilePage());
+              },
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              leading: const Icon(Icons.account_circle_rounded),
+              title: const Text(
+                "Profile",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+            ListTile(
+              onTap: () async {
+                showDialog(
+                    barrierDismissible: false,
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text("logout"),
+                        content: const Text("Are you sure want to logout?"),
+                        actions: [
+                          IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: const Icon(
+                              Icons.cancel,
+                              color: Colors.redAccent,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () async {
+                              await authServices.signOut();
+                              Navigator.of(context).pushAndRemoveUntil(
+                                  MaterialPageRoute(
+                                      builder: (context) => const LoginPage()),
+                                  (route) => false);
+                            },
+                            icon: const Icon(
+                              Icons.done,
+                              color: Colors.greenAccent,
+                            ),
+                          ),
+                        ],
+                      );
+                    });
+              },
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              leading: const Icon(Icons.exit_to_app),
+              title: const Text(
+                "LogOut",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
